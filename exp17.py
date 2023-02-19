209d8ca7192d86ac185c7f3ca7a16b7290e4765c
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 20:02:44 +0530

    Add files via upload

---
+++ exp17.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 20:01:32 2023
+
+@author: sree
+"""
+
+dic1={'hridya':10,"asma":2,"rasna":4}
+dic2={'aswin':10,"karthik":2,"shine":4}
+dic1.update(dic2)
+print("after merge:",dic1)
