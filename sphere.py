1a3aaed912a645eff3df022b7ecd4b280977064b
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 21:05:37 +0530

    Add files via upload

---
+++ sphere.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 20:58:35 2023
+
+@author: sree
+"""
+
+import math
+def area(r):
+    return 4*math.pi*r*r
+def perimeter(r):
+    return 2*math.pi*r
