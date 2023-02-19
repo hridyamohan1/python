adf8c8b1e321e9668b065969764f8737971ffbdd
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 19:25:55 +0530

    Add files via upload

---
+++ exp4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 19:23:42 2023
+
+@author: sree
+"""
+
+n=int(input("Enter how many numbers:"))
+list=[]
+for i in range(n):
+    x=int(input("Enter number:"))
+    if x<100:
+        list.append(a)
+    else:
+        list.append("over")
+print(list)
