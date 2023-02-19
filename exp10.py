65220a6f9ac23f29a7d4cde0cd19871c2e43e02f
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 19:46:29 +0530

    Add files via upload

---
+++ exp10.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 19:45:28 2023
+
+@author: sree
+"""
+
+file=input("Enter file name:")
+print("File extension is:",file.split(".")[-1])
