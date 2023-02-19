4af194d8688593ee58dab88bb115ce8b6a95479e
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 19:42:00 +0530

    Add files via upload

---
+++ exp8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 19:40:35 2023
+
+@author: sree
+"""
+
+a=input("Enter a string:")
+s=a[-1]+a[1:-1]+a[0]
+print("After interchaning:",s)
