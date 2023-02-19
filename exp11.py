d38ae49e523bbdb4c11f5906575dd00430ad8e14
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 19:48:30 +0530

    Add files via upload

---
+++ exp11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 19:47:13 2023
+
+@author: sree
+"""
+
+r=int(input("Enter radius:"))
+area=3.14*r*r
+print("Area of circle:",area)
