dc7ef9aeb838bbe2193e7aef73416562bd95a0d4
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 21:05:22 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 20:53:00 2023
+
+@author: sree
+"""
+
+import math
+def area(r):
+    return math.pi*r*r
+def perimeter(r):
+    return 2*math.pi*r
