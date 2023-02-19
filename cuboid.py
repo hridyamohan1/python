f5b3a52d94ad21def0267154b3978f4729273d5e
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 21:05:53 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 20:54:46 2023
+
+@author: sree
+"""
+
+def area(l,h,w):
+    return (2*l*h)+(2*h*w)+(2*l*w)
+def perimeter(l,h,w):
+    return 4*(l+h+w)
