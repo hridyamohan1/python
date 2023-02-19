70a4c3d7eef4b3c02d317687d7ead1caa552909c
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 19:53:34 +0530

    Add files via upload

---
+++ exp13.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 19:51:57 2023
+
+@author: sree
+"""
+
+n=int(input("Enter an integer:"))
+nn=n*10+n
+nnn=n*100+nn
+print("Result of n+nn+nnn is:",n+nn+nnn)
