a3b972f13acd6553cc9b210fdeaf81bd127e4a69
hridyamohan1 <120092982+hridyamohan1@users.noreply.github.com>
2023-02-19 21:05:00 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 20:51:50 2023
+
+@author: sree
+"""
+
+def area(l,b):
+    return l*b
+def perimeter(l,b):
+    return 2*(l+b)
